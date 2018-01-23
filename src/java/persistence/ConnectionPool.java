2a46450f02b99dab888ae142baa3cff28fef29f4
729347 <729347@ELW7IT215981.ACDM.DS.SAIT.CA>
2018-01-23 14:12:52 -0700

    Updated persistence package. The connection pool is outlined for use with JDBC

--- src/java/persistence/ConnectionPool.java
+++ src/java/persistence/ConnectionPool.java
@@ -5,10 +5,39 @@
  */
 package persistence;
 
+import javax.sql.DataSource;
+import java.sql.*;
+
 /**
  *
  * @author 729347
  */
 public class ConnectionPool {
+    private static ConnectionPool pool;
+    private static DataSource source;
     
+    /**
+     * The getInstance method creates an connection instance using JDBC
+     * @return ConnectionPool the 'pool' of connections made available
+     */
+    public ConnectionPool getInstance() {
+        return null;
+    }
+    
+    /**
+     * The getConnection method creates a connection between the application
+     * and the DataSource
+     * @return connection represents the built in java.sql package
+     */
+    public Connection getConnection() {
+        return null;
+    }
+    
+    /**
+     * The freeConnection method puts a stop to the connection
+     * @param c represents an a variable of type connection
+     */
+    public void freeConnection(Connection c) {
+       
+    }
 }
