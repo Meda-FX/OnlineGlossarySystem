63f3512525d6304a6310d7ace0ac790cbd1edcfa
727153 <727153@ELF6215730.ACDM.DS.SAIT.CA>
2018-01-24 01:56:13 -0700

    Fix documentation Fix documentation of ConnectionPool

--- src/java/persistence/ConnectionPool.java
+++ src/java/persistence/ConnectionPool.java
@@ -7,35 +7,45 @@ package persistence;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
- *
- * @author 729347
+ * ConnectionPool class is a singular connection that allow simultaneous
+ * connections to the database 
+ * @author J. Liang, F. Xiao, M. Neguse, O. McAteer, K. Goertzen
+ * @version 0.1
  */
 public class ConnectionPool {
     private static ConnectionPool pool;
     private static DataSource source;
     
     /**
-     * The getInstance method creates an connection instance using JDBC
-     * @return ConnectionPool the 'pool' of connections made available
+    * construct a ConnectionPool object to be used for database connectivity
+    */
+    private ConnectionPool() {
+    }
+    
+    
+    /**
+     * The getInstance method return a singular connection pool to the database
+     * @return the connection pool returned
      */
     public ConnectionPool getInstance() {
         return null;
     }
     
     /**
-     * The getConnection method creates a connection between the application
-     * and the DataSource
-     * @return connection represents the built in java.sql package
+     * The getConnection method creates a connection to the database
+     * @return the connection returned
      */
     public Connection getConnection() {
         return null;
     }
     
     /**
-     * The freeConnection method puts a stop to the connection
-     * @param c represents an a variable of type connection
+     * The freeConnection method stop the connection and free up the resource
+     * @param c the connection to be stopped
      */
     public void freeConnection(Connection c) {
        
