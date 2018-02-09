de4029b475dd12b343f5ec65a5ac1a2631e8582e
729347 <729347@ELW7IT215981.ACDM.DS.SAIT.CA>
2018-02-09 14:20:24 -0700

    Kyle's Test Hello changes

--- src/java/controller/servlets/SearchServlet.java
+++ src/java/controller/servlets/SearchServlet.java
@@ -31,6 +31,6 @@ public class SearchServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String Kyle = "Kyle";
+        String Kyle = "Kyle123";
     }
 }
