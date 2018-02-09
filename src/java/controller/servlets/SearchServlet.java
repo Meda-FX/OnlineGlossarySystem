6bb46ebe12c3c4ba00ce5d96937ff9b1e297b43f
715583 <715583@ELW8IT215806.ACDM.DS.SAIT.CA>
2018-02-09 12:00:43 -0700

    test sync nbproject

--- src/java/controller/servlets/SearchServlet.java
+++ src/java/controller/servlets/SearchServlet.java
@@ -21,6 +21,7 @@ public class SearchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        String action;
         getServletContext().getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
     }
     
