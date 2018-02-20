d4f7b241d4eaa14124fdab2911cac3a67a4d4e8d
715583 <715583@ELW8IT215806.ACDM.DS.SAIT.CA>
2018-02-20 13:08:52 -0700

    login servlet update

--- src/java/controller/servlets/LoginServlet.java
+++ src/java/controller/servlets/LoginServlet.java
@@ -5,12 +5,15 @@
  */
 package controller.servlets;
 
+import business.domainClasses.User;
+import business.serviceClasses.UserService;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Receive and response to requests from web client for login page 
@@ -27,6 +30,34 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String url = "/WEB-INF/login.jsp";
+        HttpSession session = request.getSession();
+        if(username==null ||username.isEmpty() || password==null ||password.isEmpty())
+        {
+            request.setAttribute("isEmpty", true);
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+            return ;
+        }
+        
+        UserService us = new UserService();
+        User user = us.checkLogin(username,password);
+        if(user == null)
+        {
+            request.setAttribute("isInvalid", true);
+            getServletContext().getRequestDispatcher(url).forward(request, response);
+            return ;
+        }
+        else
+        {
+            //user.setPassword("");
+            session.setAttribute("user", user);
+
+        }
+     //   getServletContext().getRequestDispatcher(url).forward(req, resp);
+        
         
     }
 }
