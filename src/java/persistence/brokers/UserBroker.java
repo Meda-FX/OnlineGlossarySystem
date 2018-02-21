1584b6fa87f2743ace34afb7eb2acd16790dc662
729347 <729347@ELW7IT215981.ACDM.DS.SAIT.CA>
2018-02-20 22:25:14 -0700

    getByName #1

--- src/java/persistence/brokers/UserBroker.java
+++ src/java/persistence/brokers/UserBroker.java
@@ -205,16 +205,11 @@ public class UserBroker extends Broker {
             while (rs.next()) {
 
                 user_id = (rs.getString("user_id"));
-                if (!compareName.equals(user_id)) {
-                    if (!compareName.equals("")) {
-                        users.add(user);
-                    }
-                    compareName = user_id;
-                    user = new User();
-                    user.setID(user_id);
-                    privilegeList = user.getPrivileges();
-                    courseList = user.getCourses();
-                }
+                compareName = user_id;
+                user = new User();
+                user.setID(user_id);
+                privilegeList = user.getPrivileges();
+                courseList = user.getCourses();
 
                 //DEPARTMENT
                 department = new Department();
@@ -254,8 +249,8 @@ public class UserBroker extends Broker {
                 //LISTS
                 privilegeList.add(privID);
                 courseList.add(courseCode);
+                users.add(user);
             }
-            users.add(user);
 
         } catch (SQLException ex) {
             Logger.getLogger(GlossaryEntryBroker.class.getName()).log(Level.SEVERE, "Cannot read users", ex);
@@ -449,87 +444,6 @@ public class UserBroker extends Broker {
 
     @Override
     public List<Object> getAll() {
-        ConnectionPool pool = ConnectionPool.getInstance();
-        Connection connection = pool.getConnection();
-        List<Object> users = new ArrayList<>();
-        
-        String user_id = null;
-	String password = null;
-	int department_id = 0;
-	String name = null;
-	String userEmail = null;
-	int activated = 0;
-        String deptID = null;
-        String deptName = null;
-        int privID;
-        String description;
-        String courseCode =null;
-        String courseName = null;
-        String year = null;
-        
-        Department department = null;
-        
-        String selectSQL = "SELECT * FROM [GlossaryDataBase].[dbo].[user];";
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        User user = null;
-        
-        try {
-            ps = connection.prepareStatement(selectSQL);
-            ps.setString(1, name);
-            rs = ps.executeQuery();
-            PrivilegeList privilegeList=null;
-            CourseList courseList = null;
-            privilegeList = user.getPrivileges();
-            courseList = user.getCourses();
-            while (rs.next()) {
-                //DEPARTMENT
-                department = new Department();
-                deptID = rs.getString("departmentID");
-                deptName = rs.getString("name");
-                
-                department.setDepartmentID(deptID);
-                department.setName(deptName);
-                
-                //PRIVILEGE
-                Privilege priv = new Privilege();
-                privID = rs.getInt("privilegeID");
-                description = rs.getString("description");
-                priv.setPrivilegeID(privID);
-                priv.setDescription(description);
-                privilegeList.add(privID);
-                
-                //USER
-                password = (rs.getString("password"));
-                department_id = (rs.getInt("department_id"));
-                name = (rs.getString("name"));
-                userEmail = (rs.getString("email"));
-                activated = (rs.getInt("activated"));
-                
-                user.setID(user_id);
-                user.setPassword(password);
-                user.setDepartment(department);
-                user.setName(name);
-                user.setEmail(userEmail);
-                
-                //COURSE
-                Course course = new Course();
-                courseCode = rs.getString("courseCode");
-                courseName = rs.getString("courseName");
-                year = rs.getString("year");
-                
-                //LISTS
-                privilegeList.add(privID);
-                courseList.add(courseCode);
-                
-                users.add(user);
-            }
-            
-            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-        } catch (SQLException ex) {
-            Logger.getLogger(UserBroker.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return users;
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
