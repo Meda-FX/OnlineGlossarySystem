3dd85ec6da230f7130b1f3e0c055ddd182cc8968
727153 <727153@ELF6215730.ACDM.DS.SAIT.CA>
2018-03-01 13:28:55 -0700

    Finished editing existing methods in DefinitionBroker
    spelling fix in Definition

--- src/java/persistence/brokers/DefinitionBroker.java
+++ src/java/persistence/brokers/DefinitionBroker.java
@@ -39,7 +39,7 @@ public class DefinitionBroker extends Broker {
         Definition definition = null;
         Course course;
         User user;
-        
+
         String term;
         String name;
         String userid;
@@ -51,14 +51,14 @@ public class DefinitionBroker extends Broker {
         String dictionaryContent;
         String dictionaryCitation;
         java.util.Date newDate;
-        
+
         String selectSQL = "SELECT * "
-                            + "from [dbo].[definition] "
-                            + "join [dbo].[course] "
-                            + "on (definition.course_code=course.course_code) "
-                            + "join [dbo].[user] "
-                            + "on (definition.made_by=[dbo].[user].user_id) "
-                            + "where definition_uid = ?";
+                + "from [dbo].[definition] "
+                + "join [dbo].[course] "
+                + "on (definition.course_code=course.course_code) "
+                + "join [dbo].[user] "
+                + "on (definition.made_by=[dbo].[user].user_id) "
+                + "where definition_uid = ?";
 
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -68,13 +68,13 @@ public class DefinitionBroker extends Broker {
             ps.setInt(1, did);
             rs = ps.executeQuery();
             while (rs.next()) {
-                definition= new Definition();
+                definition = new Definition();
                 term = rs.getString("glossary_entry");
                 content = rs.getString("definition");
                 newDate = new java.util.Date(rs.getTimestamp("date_created").getTime());
                 citation = rs.getString("citation");
                 name = rs.getString("name");
-                userid=rs.getString("user_id");
+                userid = rs.getString("user_id");
                 dictionaryContent = rs.getString("dictionary_definition");
                 dictionaryCitation = rs.getString("dictionary_citation");
                 user = new User();
@@ -86,12 +86,19 @@ public class DefinitionBroker extends Broker {
                 course.setCourseCode(courseId);
                 course.setCourseName(course_name);
                 // definition = new definition(user,)
-                definition = new Definition(user, newDate, citation, dictionaryCitation, 
+                definition = new Definition(user, newDate, citation, dictionaryCitation,
                         course, content, dictionaryContent, term);
             }
 
         } catch (SQLException ex) {
-            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, "Fail to read definition", ex);
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+            } catch (SQLException e) {
+            }
+            pool.freeConnection(connection);
         }
         return definition;
     }
@@ -103,14 +110,14 @@ public class DefinitionBroker extends Broker {
      * @return a list of definitions by course.
      */
     public List<Definition> getByCourse(Course course) {
-          ConnectionPool pool = ConnectionPool.getInstance();
+        ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
         ArrayList<Definition> delist = new ArrayList<>();
-     
+
         Definition definition = null;
-      //  Course course;
+        //  Course course;
         User user;
-        
+
         String term;
         String name;
         String userid;
@@ -124,10 +131,10 @@ public class DefinitionBroker extends Broker {
         String dictionaryCitation;
 
         String selectSQL = "SELECT * "
-                            + "from [dbo].[definition] "
-                            + "join [dbo].[user] "
-                            + "on (definition.made_by=[dbo].[user].user_id) "
-                            + "where [dbo].[definition].course_code = ?";
+                + "from [dbo].[definition] "
+                + "join [dbo].[user] "
+                + "on (definition.made_by=[dbo].[user].user_id) "
+                + "where [dbo].[definition].course_code = ?";
 
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -144,23 +151,29 @@ public class DefinitionBroker extends Broker {
                 dictionaryContent = rs.getString("dictionary_definition");
                 dictionaryCitation = rs.getString("dictionary_citation");
                 name = rs.getString("name");
-                userid=rs.getString("user_id");
+                userid = rs.getString("user_id");
                 user = new User();
                 user.setID(userid);
                 user.setName(name);
                 // definition = new definition(user,)
-                definition = new Definition(user, newDate, citation, 
-                        dictionaryCitation, course, content, 
+                definition = new Definition(user, newDate, citation,
+                        dictionaryCitation, course, content,
                         dictionaryContent, term);
                 delist.add(definition);
             }
 
         } catch (SQLException ex) {
-            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, "Fail to read definition", ex);
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+            } catch (SQLException e) {
+            }
+            pool.freeConnection(connection);
         }
         return delist;
     }
-
 
     /**
      * The getByUser method returns a list of definitions related to the user.
@@ -168,7 +181,6 @@ public class DefinitionBroker extends Broker {
      * @param user represents the account in the user table in the database.
      * @return a list of definitions by user.
      */
-    
     public List<Definition> getByUser(User user) {
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
@@ -187,10 +199,10 @@ public class DefinitionBroker extends Broker {
         String dictionaryCitation;
 
         String selectSQL = "SELECT * "
-                            + "from [dbo].[definition] "
-                            + "join [dbo].[course] "
-                            + "on (definition.course_code=course.course_code) "
-                            + "where made_by =?";
+                + "from [dbo].[definition] "
+                + "join [dbo].[course] "
+                + "on (definition.course_code=course.course_code) "
+                + "where made_by =?";
 
         PreparedStatement ps = null;
         ResultSet rs = null;
@@ -214,18 +226,24 @@ public class DefinitionBroker extends Broker {
                 dictionaryContent = rs.getString("dictionary_definition");
                 dictionaryCitation = rs.getString("dictionary_citation");
                 // definition = new definition(user,)
-                definition = new Definition(user, newDate, citation, 
-                        dictionaryCitation, course, content, 
+                definition = new Definition(user, newDate, citation,
+                        dictionaryCitation, course, content,
                         dictionaryContent, term);
                 delist.add(definition);
             }
 
         } catch (SQLException ex) {
-            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, "Fail to read definition", ex);
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+            } catch (SQLException e) {
+            }
+            pool.freeConnection(connection);
         }
         return delist;
     }
-
 
     /**
      * The getByGlossaryEntry returns a list of definitions related to the entry
@@ -234,7 +252,6 @@ public class DefinitionBroker extends Broker {
      * @param glossary represents an entry in the glossary.
      * @return a list of definitions by glossary entry.
      */
-
     public DefinitionList getByGlossaryEntry(GlossaryEntry glossary) {
         ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
@@ -242,12 +259,12 @@ public class DefinitionBroker extends Broker {
         ResultSet rs = null;
         DefinitionList list = new DefinitionList();
         String selectSQL = "SELECT * "
-                            + "FROM [GlossaryDataBase].[dbo].[definition] "
-                            + "join [dbo].[course] "
-                            + "on (definition.course_code=course.course_code) "
-                            + "join [dbo].[user] "
-                            + "on (definition.made_by=[dbo].[user].user_id) "
-                            + "where glossary_entry = ?;";
+                + "FROM [GlossaryDataBase].[dbo].[definition] "
+                + "join [dbo].[course] "
+                + "on (definition.course_code=course.course_code) "
+                + "join [dbo].[user] "
+                + "on (definition.made_by=[dbo].[user].user_id) "
+                + "where glossary_entry = ?";
         Definition definition;
         Course course;
         User user;
@@ -266,14 +283,14 @@ public class DefinitionBroker extends Broker {
             ps = connection.prepareCall(selectSQL);
             ps.setString(1, glossary.getTerm());
             rs = ps.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 definitionUid = rs.getString("definition_uid");
                 glossaryEntry = rs.getString("glossary_entry");
                 content = rs.getString("definition");
                 dateCreated = new Date(rs.getTimestamp("date_created").getTime());
                 citation = rs.getString("citation");
                 madeBy = rs.getString("made_by");
-                courseId = rs.getString("course_id");       
+                courseId = rs.getString("course_id");
                 dictionaryContent = rs.getString("dictionary_definition");
                 dictionaryCitation = rs.getString("dictionary_citation");
                 name = rs.getString("name");
@@ -285,26 +302,33 @@ public class DefinitionBroker extends Broker {
                 course = new Course();
                 course.setCourseCode(courseId);
                 course.setCourseName(course_name);
-                definition = new Definition(user, dateCreated, citation, dictionaryCitation, 
+                definition = new Definition(user, dateCreated, citation, dictionaryCitation,
                         course, content, dictionaryContent, glossary.getTerm());
                 list.add(definition);
             }
         } catch (SQLException e) {
-            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, null, e);
+            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, "Fail to read definition", e);
 
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+            } catch (SQLException e) {
+            }
+            pool.freeConnection(connection);
         }
 
         return list;
     }
+
     @Override
     public int delete(Object object) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-         ConnectionPool pool = ConnectionPool.getInstance();
+        ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
 
         Definition definition = (Definition) object;
         String sql = "DELETE FROM definition WHERE definition_uid=?";
-               
 
         PreparedStatement ps = null;
 
@@ -314,26 +338,36 @@ public class DefinitionBroker extends Broker {
             ps = connection.prepareStatement(sql);
             ps.setString(1, definition.getDefinitionID());
 
-
             affectRows = ps.executeUpdate();
             // may need to update the definition edit log
+
+            if (affectRows > 0) {
+                return 1;
+            }
         } catch (SQLException ex) {
-            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, "Fail to delete definition", ex);
+        } finally {
+            try {
+                ps.close();
+            } catch (SQLException ex) {
+
+            }
+            pool.freeConnection(connection);
         }
 
-        return affectRows;
+        return 0;
     }
 
     @Override
     public int update(Object object) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-         ConnectionPool pool = ConnectionPool.getInstance();
+        ConnectionPool pool = ConnectionPool.getInstance();
         Connection connection = pool.getConnection();
 
         Definition definition = (Definition) object;
-        String sql = "UPDATE definition \n"
+        String sql = "UPDATE definition "
                 + "SET (definition=?,dictionary_definition=?,"
-                + "citation=?,dictionary_citation=?,made_by=?,course_code=?)\n"
+                + "citation=?,dictionary_citation=?,made_by=?,course_code=?) "
                 + "WHERE definition_uid=?";
 
         PreparedStatement ps = null;
@@ -344,8 +378,8 @@ public class DefinitionBroker extends Broker {
             ps = connection.prepareStatement(sql);
             //ps.setString(1, definition.getTerm());
             ps.setString(1, definition.getContent());
-            ps.setString(2, definition.getDictionContent());
-         //   ps.setDate(3, new java.sql.Date(new java.util.Date().getTime()));
+            ps.setString(2, definition.getDictionaryContent());
+            //   ps.setDate(3, new java.sql.Date(new java.util.Date().getTime()));
             ps.setString(3, definition.getCitation());
             ps.setString(4, definition.getDictionaryCitation());
             ps.setString(5, definition.getWrittenBy().getID());
@@ -353,19 +387,28 @@ public class DefinitionBroker extends Broker {
 
             affectRows = ps.executeUpdate();
             // may need to update the definition edit log
+            if (affectRows > 0) {
+                return 1;
+            }
         } catch (SQLException ex) {
-            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, "Fail to update definition", ex);
+        } finally {
+            try {
+                ps.close();
+            } catch (SQLException ex) {
+
+            }
+            pool.freeConnection(connection);
         }
 
-        return affectRows;
+        return 0;
     }
 
     @Override
     public List<Object> getAll() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
+
     @Override
     public int insert(Object object) {
         // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -374,8 +417,9 @@ public class DefinitionBroker extends Broker {
 
         Definition definition = (Definition) object;
         String sql = "INSERT INTO definition \n"
-                + "(glossary_entry,definition, date_created,citation,made_by,course_code,[type])\n"
-                + "VALUES (?,?,?,?,?,?,?)";
+                + "(definition_uid, glossary_entry, definition, dictionary_definition,"
+                + "date_created, citation, dictionary_citation, made_by,course_code) "
+                + "VALUES (?,?,?,?,?,?,?,?,?)";
 
         PreparedStatement ps = null;
 
@@ -383,20 +427,32 @@ public class DefinitionBroker extends Broker {
 
         try {
             ps = connection.prepareStatement(sql);
-            ps.setString(1, definition.getTerm());
-            ps.setString(2, definition.getContent());
-            ps.setDate(3, new java.sql.Date(new java.util.Date().getTime()));
-            ps.setString(4, definition.getCitation());
-            ps.setString(5, definition.getWrittenBy().getID());
-            ps.setString(6, definition.getDefinitionType() + "");
+            ps.setString(1, definition.getDefinitionID());
+            ps.setString(2, definition.getTerm());
+            ps.setString(3, definition.getContent());
+            ps.setString(4, definition.getDictionaryContent());
+            ps.setDate(5, new java.sql.Date(new java.util.Date().getTime()));
+            ps.setString(6, definition.getCitation());
+            ps.setString(7, definition.getDictionaryCitation());
+            ps.setString(8, definition.getWrittenBy().getID());
+            ps.setString(9, definition.getCourse().getCourseCode());
 
             affectRows = ps.executeUpdate();
             // may need to update the definition edit log
+            if (affectRows > 0) {
+                return 1;
+            }
         } catch (SQLException ex) {
-            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(DefinitionBroker.class.getName()).log(Level.SEVERE, "Fail to insert definition", ex);
+        } finally {
+            try {
+                ps.close();
+            } catch (SQLException ex) {
+
+            }
+            pool.freeConnection(connection);
         }
 
-        return affectRows;
+        return 0;
     }
 }
-
