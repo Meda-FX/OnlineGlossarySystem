236d53877c0a3a9e9513f189f0f32b1a8ee83b10
684243 <684243@ELF6IT215871.ACDM.DS.SAIT.CA>
2018-03-01 13:46:20 -0700

    Created TR_acc_req_insert_after trigger

---
+++ src/java/database/triggers.sql
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  684243
+ * Created: Mar 1, 2018
+ */
+
+CREATE TRIGGER TR_acc_req_insert_after
+ON account_request
+AFTER INSERT AS
+BEGIN
+SET IDENTITY_INSERT dbo.account_request_log ON
+INSERT INTO account_request_log(request_id,request_date,request_by,request_log_type)
+select request_id,request_date,request_by,request_type from INSERTED
+SET IDENTITY_INSERT dbo.account_request_log OFF
+
+END
+
+CREATE TRIGGER TR_acc_req_insert_before
+ON account_request
+BEFORE INSERT AS
+BEGIN
+DELETE FROM account_request
+where 
+END
