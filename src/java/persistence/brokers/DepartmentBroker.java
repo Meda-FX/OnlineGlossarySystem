e0bde2732681cb7a8406c2d3eb5f7a4c8aae3e28
729347 <729347@ELW7IT215981.ACDM.DS.SAIT.CA>
2018-03-09 08:04:37 -0700

    DepartmentBroker getByID finished.
    Insert, update, delete and getAll in progress.

--- src/java/persistence/brokers/DepartmentBroker.java
+++ src/java/persistence/brokers/DepartmentBroker.java
@@ -1,7 +1,20 @@
 package persistence.brokers;
 
+import business.domainClasses.Definition;
+import business.domainClasses.DefinitionList;
 import business.domainClasses.Department;
+import business.domainClasses.GlossaryEntry;
+import business.domainClasses.User;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import persistence.ConnectionPool;
 
 /**
  * The DepartmentBroker provide connectivity to the database and allow insertion,
@@ -17,8 +30,44 @@ public class DepartmentBroker extends Broker{
      * @param departmentID represents the ID of a department.
      * @return a Department object from the Department database.
      */
-    public Department getByID(String departmentID) {
-        return null;
+    public Department getByID(int departmentID) {
+        ConnectionPool pool = ConnectionPool.getInstance();
+        Connection connection = pool.getConnection();
+        
+        String selectSQL = "SELECT "
+                + " [GlossaryDataBase].[dbo].[department].department_id, "
+                + " [GlossaryDataBase].[dbo].[department].name"
+                + " FROM [GlossaryDataBase].[dbo].[department]"
+                + " WHERE [GlossaryDataBase].[dbo].[department].department_id = ?";
+        
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        
+        Department dept = null;
+        int dept_ID = 0;
+        String dept_NAME = null;
+
+        try {
+            ps = connection.prepareStatement(selectSQL);
+            ps.setInt(1, departmentID);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                dept_ID = rs.getInt("department_id");
+                dept_NAME = rs.getString("name");
+            }
+            
+            dept = new Department(dept_ID, dept_NAME);
+        } catch (SQLException ex) {
+            Logger.getLogger(GlossaryEntryBroker.class.getName()).log(Level.SEVERE, "Cannot read users", ex);
+        } finally {
+            try {
+                rs.close();
+                ps.close();
+            } catch (SQLException ex) {
+            }
+            pool.freeConnection(connection);
+        }
+        return dept;
     }
 
     @Override
