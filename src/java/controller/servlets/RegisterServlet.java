ab7d0f3a10c4493b806b1adeea61fcc6910cd872
715583 <715583@ELW8IT215806.ACDM.DS.SAIT.CA>
2018-03-09 08:28:25 -0700

    register function

--- src/java/controller/servlets/RegisterServlet.java
+++ src/java/controller/servlets/RegisterServlet.java
@@ -5,6 +5,7 @@
  */
 package controller.servlets;
 
+import business.serviceClasses.UserService;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
@@ -23,6 +24,7 @@ public class RegisterServlet extends HttpServlet {
             throws ServletException, IOException {
 
         String url = "/WEB-INF/register.jsp";
+        //ajax to check the field
         getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 
@@ -32,9 +34,32 @@ public class RegisterServlet extends HttpServlet {
         String fname = request.getParameter("fname");
         String lname = request.getParameter("lname");
         String email = request.getParameter("email");
-        String student_id = request.getParameter("#"); //need to confirm the attribute name
-        String password = request.getParameter("password");
-        String password_confirm =request.getParameter("#"); //need to confirm the attribute name
-
+        String student_id = request.getParameter("studentId"); 
+        String password = request.getParameter("pass");
+        String password_confirm =request.getParameter("confirmPass"); 
+        Boolean confirm_regis=false; //false means not registered
+        UserService us = new UserService();
+        String url = "/WEB-INF/register.jsp";
+        
+        if(fname!=null &&!fname.equals("") && 
+                lname!=null &&!lname.equals("") &&
+                email!=null &&!email.equals("") &&
+                student_id!=null &&!student_id.equals("") &&
+                password!=null &&!password.equals("") &&
+                password_confirm!=null &&!password_confirm.equals(""))
+        {
+            confirm_regis=us.registerUser(fname+" "+lname,email,student_id,password,password_confirm);
+        }
+        
+        if(confirm_regis==false) 
+        {
+            //display error message
+        }
+        else 
+        {
+            //display message to check their email
+        }
+        
+        getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 }
