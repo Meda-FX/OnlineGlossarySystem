10c4be0a889e18a9a566ef1d1a21523e33e367d8
715583 <715583@ELW8IT215806.ACDM.DS.SAIT.CA>
2018-03-23 16:36:27 -0600

    edit servlet debugging! ajax from manage term

--- web/resources/js/script.js
+++ web/resources/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
  
   
     
-    $("#editform").submit(function(event) {
+    $(".editform").submit(function(event) {
         $.get($(this).attr("action"), $(this).serialize(), function(responseJson) {
             $("#term").val(responseJson.term);
             $("#termId").val(responseJson.definitionID);
