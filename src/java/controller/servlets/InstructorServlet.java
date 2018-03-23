eac39deddc713d7d521b37d800bff3686f31bf39
684243 <684243@ELF6IT215871.ACDM.DS.SAIT.CA>
2018-03-23 09:22:51 -0600

    Working on InstructorServlet to allow adding of terms

--- src/java/controller/servlets/InstructorServlet.java
+++ src/java/controller/servlets/InstructorServlet.java
@@ -5,11 +5,17 @@
  */
 package controller.servlets;
 
+import business.domainClasses.Course;
+import business.domainClasses.Definition;
 import business.domainClasses.GlossaryEntry;
 import business.domainClasses.User;
+import business.serviceClasses.CourseService;
+import business.serviceClasses.DefinitionService;
 import business.serviceClasses.GlossaryEntryService;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -29,15 +35,15 @@ public class InstructorServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-        String url = "/WEB-INF/_instructor/instructor.jsp";
 
+        String url = "/WEB-INF/_instructor/instructor.jsp";
 
         HttpSession session = request.getSession();
         GlossaryEntryService ges = new GlossaryEntryService();
-        
-        User user = (User)session.getAttribute("user");
+
+        User user = (User) session.getAttribute("user");
         List<GlossaryEntry> termList = ges.getByUser(user.getID());
+
         getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 
@@ -46,21 +52,84 @@ public class InstructorServlet extends HttpServlet {
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         String action = request.getParameter("newTerm");
-        String term = request.getParameter("terms");
+        String term = request.getParameter("term");
         String definition = request.getParameter("definition");
         String citation = request.getParameter("citation");
-        String defDict = request.getParameter("defDict");
+        String defDict = request.getParameter("defDefinition");
         String defCita = request.getParameter("defCita");
+        String course = request.getParameter("course");
+        String errorMessage = "The following fields are blank / invalid";
+        String url = "/WEB-INF/_instructor/instructor.jsp";
+        Timestamp date = new Timestamp(new Date().getTime());
+        DefinitionService ds = new DefinitionService();
+        CourseService cS = new CourseService();
+        Course newCourse = cS.get(course);
+        Definition newEntry = new Definition();
+        User user = (User) session.getAttribute("user");
 
-        
-        if (action.compareTo("newTerm") == 1) {
-            
-            
-        } else if (action.compareTo("save") == 1) {
-            
-        } else if (action.compareTo("cancel") == 1) {
-            
+        if (term == null) {
+            errorMessage += " Term";
+        }
+        if (definition == null) {
+            errorMessage += " definition";
+        }
+        if (defDict == null) {
+            errorMessage += " dictionary definition";
+        }
+        if (defCita == null) {
+            errorMessage += " dictionary citation";
+        }
+        if (course == null) {
+            errorMessage += " course";
         }
 
+        newEntry.setCitation(citation);
+        newEntry.setContent(definition);
+        newEntry.setCourse(newCourse);
+        newEntry.setDictionaryContent(defDict);
+        newEntry.setDictionaryCitation(defCita);
+        newEntry.setTerm(term);
+        newEntry.setWrittenBy((User) session.getAttribute("user"));
+
+        if (action.compareTo("newTerm") == 1) {
+            // checking to see if any of the required terms are null
+            if (term != null && definition != null && defDict != null
+                    && defCita != null && course != null) {
+                request.setAttribute("message", errorMessage);
+                getServletContext().getRequestDispatcher(url).forward(request, response);
+            }
+            newEntry.setStatus("pending review");
+            //This is checking to see if the term already exists on the 
+            //GlossaryEntry table. If it doesn't already exit the program will
+            //Create the entry
+            if (checkGlossaryEntry(newEntry) == false) {
+                GlossaryEntryBroker ges = new GlossaryEntryBroker();
+                GlossaryEntry newGlossaryEntry = new GlossaryEntry(date, term, user);
+                if (ges.insert(newGlossaryEntry) == 0) 
+                    errorMessage = "Error something went wrong with the glossary entry!";
+            } 
+            //Remove all of the something went wrongs!!!!
+            if(ds.insert(newEntry) == 1){
+                errorMessage = "Your term is now pending review!";
+            }else {
+                errorMessage = "Something went wrong with the term Entry";
+            }
+            request.setAttribute("message",errorMessage);
+            
+        } else if (action.compareTo("save") == 1) {
+            newEntry.setStatus("In progress");
+        }
+
+    }
+
+    public boolean checkGlossaryEntry(Definition definition) {
+        GlossaryEntryService ges = new GlossaryEntryService();
+        List<GlossaryEntry> gesList = ges.getMatched(definition.getTerm());
+        for (int i = 0; i < gesList.size(); i++) {
+            if (definition.getTerm().equals(gesList.get(i).getTerm()) == true) {
+                return true;
+            }
+        }
+        return false;
     }
 }
