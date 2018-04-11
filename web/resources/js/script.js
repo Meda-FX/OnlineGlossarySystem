fabc7a374875890f4fc7563177a5ec29282e0b7c
715583 <715583@ELW8IT215806.ACDM.DS.SAIT.CA>
2018-04-10 23:54:26 -0600

    js update

--- web/resources/js/script.js
+++ web/resources/js/script.js
@@ -1,31 +1,31 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//    $("#end").prop("max", moment().format("YYYY-MM-DD")); 
-//    $("#start").prop("max", moment().format("YYYY-MM-DD")); 
-    
-    $("#start").change(function(){
-       $("#end").prop("min", $(this).val()); 
+    $("#end").prop("max", moment().format("YYYY-MM-DD"));
+    $("#start").prop("max", moment().format("YYYY-MM-DD"));
+
+    $("#start").change(function () {
+        $("#end").prop("min", $(this).val());
     });
 
-    $("#end").change(function(){
-       $("#start").prop("max", $(this).val()); 
+    $("#end").change(function () {
+        $("#start").prop("max", $(this).val());
     });
 
-    $(".editForm").submit(function(event) {
-        $.get($(this).attr("action"), $(this).serialize(), function(responseJson) {
+    $(".editForm").submit(function (event) {
+        $.get($(this).attr("action"), $(this).serialize(), function (responseJson) {
             $("#term").val(responseJson.term);
             $("#termId").val(responseJson.definitionID);
             $("#definition").val(responseJson.content);
             $("#citation").val(responseJson.citation);
             $("#defDefinition").val(responseJson.dictionaryContent);
-            $("#dicCitation").val(responseJson.dictionaryCitation);           
+            $("#dicCitation").val(responseJson.dictionaryCitation);
         });
         event.preventDefault();
     });
-        
-    
-     $(".editUsers").submit(function(event) {
-        $.get($(this).attr("action"), $(this).serialize(), function(responseJson) {
+
+
+    $(".editUsers").submit(function (event) {
+        $.get($(this).attr("action"), $(this).serialize(), function (responseJson) {
             $("#userId").val(responseJson.id);
             $("#userId").prop("readonly", true);
             $("#userName").val(responseJson.name);
@@ -37,23 +37,24 @@ $(document).ready(function(){
             } else {
                 $("#inactive").prop("checked", true);
             }
-            $("#privList").children().each(function(){
+            $("#privList").children().each(function () {
                 $(this).prop("checked", false);
             });
-            responseJson.privileges.privileges.forEach(function(item){
-               $("#privList").children().eq(item.privilegeID-1).prop("checked", true);
+            responseJson.privileges.privileges.forEach(function (item) {
+                $("#privList").children().eq(item.privilegeID - 1).prop("checked", true);
             });
         });
         event.preventDefault();
     });
-    
-    $(".editInstructorForm").submit(function(event) {
-        $.get($(this).attr("action"), $(this).serialize(), function(responseJson) {
+
+    $(".editInstructorForm").submit(function (event) {
+        $.get($(this).attr("action"), $(this).serialize(), function (responseJson) {
             $("#term").val(responseJson.term);
             $("#termId").val(responseJson.definitionID);
             $("#definition").val(responseJson.content);
             $("#citation").val(responseJson.citation);
             $("#defDefinition").val(responseJson.dictionaryContent);
+
             $("#dicCitation").val(responseJson.dictionaryCitation);   
             var courseToSet = responseJson.course.courseCode;
             
@@ -62,11 +63,15 @@ $(document).ready(function(){
  //           var courselist = document.getElementById("#courseCode");
 
     //        $("#courseCode").val(responseJson.course.courseCode);           
+
+            $("#dicCitation").val(responseJson.dictionaryCitation);
+            $("#courseCode").val(responseJson.course.courseCode);
+
         });
         event.preventDefault();
     });
-    
-    $(".newUser").click(function(){
+
+    $(".newUser").click(function () {
         $("#userId").val("");
         $("#userId").prop("readonly", false);
         $("#userName").val("");
@@ -75,54 +80,139 @@ $(document).ready(function(){
         $("#inactive").prop("checked", false);
         $("#active").attr("disabled", true);
         $("#inactive").attr("disabled", true);
-        $("#privList").children().each(function(){
+        $("#privList").children().each(function () {
             $(this).prop("checked", false);
         });
     });
 
-    $(".newTerm").click(function(){
+    $(".newTerm").click(function () {
         $("#term").val("");
         $("#termId").val("");
         $("#definition").val("");
         $("#citation").val("");
         $("#defDefinition").val("");
-        $("#dicCitation").val("");           
+        $("#dicCitation").val("");
         $("#courseCode").val("");
     });
 
     //CRUD hide and show script
-    $("#crud").hide(); 
-    $(".hideFormTerm").click(function(){
-           $("#crud").hide(1000);
-       }); 
-    $(".addTerms").click(function(){
-           $("#crud").show(1000);
-       });
-  
-    //Read More and Read Less script
-     $('.readMoreContent').addClass('hide');
-     $('.readMore, .readLess').removeClass('hide');
-
-     $('.readMore').on('click', function(e) {
-       $(this).next('.readMoreContent').removeClass('hide');
-       $(this).addClass('hide');
-       e.preventDefault();
-     });
-
-    $('.readLess').on('click', function(e) {
-      var p = $(this).parents('.readMoreContent');
-      p.addClass('hide');
-      p.prev('.readMore').removeClass('hide'); // Hide only the preceding "Read More"
-      e.preventDefault();
+    $("#crud").hide();
+    $(".hideFormTerm").click(function () {
+        $("#crud").hide(1000);
     });
-   
-   //Navigation active page locator 
-   var path = window.location.href;
-   $('.navbar ul a').each(function(){
-       if (this.href === path) {
-           $(this).addClass('active');
-       }
-   });
-        
- });
- 
+    $(".addTerms").click(function () {
+        $("#crud").show(1000);
+    });
+
+    //Read More and Read Less script
+    $('.readMoreContent').addClass('hide');
+    $('.readMore, .readLess').removeClass('hide');
+
+    $('.readMore').on('click', function (e) {
+        $(this).next('.readMoreContent').removeClass('hide');
+        $(this).addClass('hide');
+        e.preventDefault();
+    });
+
+    $('.readLess').on('click', function (e) {
+        var p = $(this).parents('.readMoreContent');
+        p.addClass('hide');
+        p.prev('.readMore').removeClass('hide'); // Hide only the preceding "Read More"
+        e.preventDefault();
+    });
+
+    //Navigation active page locator 
+    var path = window.location.href;
+    $('.navbar ul a').each(function () {
+        if (this.href === path) {
+            $(this).addClass('active');
+        }
+    });
+
+    $(".reportForm").submit(function (event) {
+        $.get($(this).attr("action"), $(this).serialize(), function (responseJson) {
+
+            var data = responseJson.map(function (item) {
+                return {x: moment(new Date(item["date"])).format("YYYY-MM-DD"), y: item["count"]};
+            });
+            var type;
+
+            var startDate = moment($("#start").val());
+            var endDate = moment($("#end").val());
+            var daysMagnitude = endDate.diff(startDate, 'days');
+
+            if ($("#reportType").val() === "1")
+                type = 'Registrations';
+            else if ($("#reportType").val() === "2")
+                type = 'Logins';
+
+            var title = "Daily " + type + " between " + startDate.format("YYYY-MM-DD") + " and " + endDate.format("YYYY-MM-DD");
+            var timescale;
+            if (daysMagnitude < 14) {
+                timescale = 'day';
+            } else if (daysMagnitude < 28 * 4) {
+                timescale = 'week';
+            } else if (daysMagnitude < 365) {
+                timescale = 'month';
+            } else if (daysMagnitude < 365 * 4) {
+                timescale = 'quarter';
+            } else {
+                timescale = 'year';
+            }
+
+            var chart = new Chart($('#canvas'), {
+                type: 'line',
+                data: {
+                    datasets: [{
+                            data: data,
+                            borderColor: '#da291c',
+                            fill: false
+                        }]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    responsive: true,
+                    title: {
+                        display: true,
+                        text: title,
+                        fontColor: '#da291c',
+                        fontSize: 24
+                    },
+                    scales: {
+                        xAxes: [{
+                                gridLines: {
+                                    display: false,
+                                    drawBorder: false
+                                },
+                                type: "time",
+                                time: {
+                                    unit: timescale,
+                                    distribution: 'linear',
+                                    max: endDate,
+                                    min: startDate
+                                },
+                                ticks: {
+                                    source: 'auto'
+                                }
+                            }],
+                        yAxes: [{
+                                gridLines: {
+                                    drawBorder: false
+                                },
+                                type: "linear",
+                                bounds: 'data',
+                                ticks: {
+                                    beginAtZero: true,
+                                    source: 'auto'
+                                }
+                            }]
+                    }}
+            });
+            chart.update();
+        });
+        event.preventDefault();
+    });
+});
+
